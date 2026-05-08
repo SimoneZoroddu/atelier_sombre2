ee999fb74bf4788ad502e0c0ca2f90d1e314a030
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-05-08 17:07:06 +0200

    add cart link to the navbar

--- front_end/src/components/AppHeader.jsx
+++ front_end/src/components/AppHeader.jsx
@@ -27,7 +27,7 @@ export default function AppHeader() {
                                 <Link className="nav-link" href="#">Pricing</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" href="#">Cart</Link>
+                                <Link className="nav-link" to="/cart">Cart</Link>
                             </li>
                         </ul>
                     </div>
