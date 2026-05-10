273a0ad7f42d5256b4e23b0ef33b0eac91d80b5a
SimoneZoroddu <lenviur@gmail.com>
2026-05-10 18:20:27 +0200

    Refactor AppFooter and AppHeader components for improved structure and styling; update AppHome layout for better responsiveness

--- front_end/src/components/AppFooter.jsx
+++ front_end/src/components/AppFooter.jsx
@@ -7,20 +7,20 @@ export default function AppFooter() {
 
 
     return (
-        <div className="container p-4">
+        <footer className="container-fluid p-4 mt-4 background_color_basic"  >
             <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-5">
                 <div className="col">
                     <h4 className="pb-4">Resta aggiornato!</h4>
                     <form action="">
                         <div>
                             <input className="rounded-pill border-1 p-1 px-2" name="emailNewsletter" type="email" placeholder="Inserisci l'email" required />
-                            <button className="bg-white border-0">Invia</button>
+                            <button className="border-0 background_color_basic ms-3">Invia</button>
                         </div>
                     </form>
                 </div>
                 <div className="col">
                     <ul className="list-unstyled">
-                        <li className="fw-medium">Servizio Clienti</li>
+                        <li className="fw-medium fs-4">Servizio Clienti</li>
                         <li><Link href="" className="text-black underline_hover">Contattaci</Link></li>
                         <li><a href="" className="text-black underline_hover">Servizi Premium</a></li>
                         <li><a href="" className="text-black underline_hover">Traccia il mio ordine</a></li>
@@ -32,14 +32,14 @@ export default function AppFooter() {
                 </div>
                 <div className="col">
                     <ul className="list-unstyled">
-                        <li className="fw-medium">Store</li>
+                        <li className="fw-medium fs-4">Store</li>
                         <li><a href="" className="text-black underline_hover">Boutiques</a></li>
 
                     </ul>
                 </div>
                 <div className="col">
                     <ul className="list-unstyled">
-                        <li className="fw-medium">Informazioni aziendali</li>
+                        <li className="fw-medium fs-4">Informazioni aziendali</li>
                         <li><a href="" className="text-black underline_hover">Tods Group</a></li>
                         <li><a href="" className="text-black underline_hover">Note legali</a></li>
                         <li><a href="" className="text-black underline_hover">Termini e Condizioni di utilizzo</a></li>
@@ -52,18 +52,17 @@ export default function AppFooter() {
                 </div>
                 <div className="col">
                     <ul className="list-unstyled">
-                        <li className="fw-medium">Seguici</li>
-                        <li><a href="" className="text-black underline_hover">Instagram</a></li>
-                        <li><a href="" className="text-black underline_hover">Facebook</a></li>
-                        <li><a href="" className="text-black underline_hover">TikTok</a></li>
-                        <li><a href="" className="text-black underline_hover">Youtube</a></li>
-                        <li><a href="" className="text-black underline_hover">Pinterest</a></li>
-                        <li><a href="" className="text-black underline_hover">LinkedIn</a></li>
-                        <li><a href="" className="text-black underline_hover">Twitter</a></li>
+                        <li className="fw-medium fs-4">Seguici</li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-instagram pe-2"></i>Instagram</a></li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-facebook pe-2"></i>Facebook</a></li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-tiktok pe-2"></i>TikTok</a></li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-youtube pe-2"></i>Youtube</a></li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-pinterest pe-2"></i>Pinterest</a></li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-linkedin pe-2"></i>LinkedIn</a></li>
+                        <li><a href="" className="text-black underline_hover"><i className="bi bi-twitter-x pe-2"></i>Twitter</a></li>
                     </ul>
                 </div>
             </div>
-        </div>
-
+        </footer>
     )
 }
