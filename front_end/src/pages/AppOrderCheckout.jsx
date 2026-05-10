b135f1691d57c0f13df17923673cbbce70eff2e6
Francesca Golzio <francescagolzio@gmail.com>
2026-05-10 11:27:35 +0200

    add payment placeholder

--- front_end/src/pages/AppOrderCheckout.jsx
+++ front_end/src/pages/AppOrderCheckout.jsx
@@ -21,7 +21,7 @@ export default function AppOrderCheckout() {
   function handleInputChange(e) {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
-   
+
   }
 
   function handleUserDataSubmit(e) {
@@ -53,10 +53,12 @@ export default function AppOrderCheckout() {
 
   return (
     <>
-      <h1>Checkout page</h1>
       <div className="container">
-        {/* Get user data */} {/* ⚠️ todo: ?? componentize to allow navigate between forms */}
+        {/* Get user data */} {/* ⚠️ todo: ?? visibility on/off or collapse or tabs to navigate between forms, ?? componentize */}
         <form className="user_data" onSubmit={handleUserDataSubmit}>
+          <div className="m-3">
+            <h2>Dati personali</h2>
+          </div>
           <div className="row row-cols-2 m-3">
             <div className="mb-3">
               <label htmlFor="firstname_field" className="form-label">Nome</label>
@@ -124,19 +126,41 @@ export default function AppOrderCheckout() {
           </div>
         </form>
 
-        <form>
-
-        </form>
-
+        {/* Payment placeholder */}
+        <div className="m-3">
+          <h3>Seleziona il metodo di pagamento</h3>
+          <div className="m-3">
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1" />
+              <label className="form-check-label" htmlFor="radioDefault1">
+                <i className="bi bi-credit-card-fill"></i> Carta di credito
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
+              <label className="form-check-label" htmlFor="radioDefault2">
+                <i className="bi bi-apple"></i> Apple Pay
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
+              <label className="form-check-label" htmlFor="radioDefault2">
+                <i className="payment-icon g-pay-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
+                </svg></i> Google Pay
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
+              <label className="form-check-label" htmlFor="radioDefault2">
+                <i className="bi bi-paypal"></i> PayPal
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
 
     </>
   )
-  {/* <div className="d-flex justify-content-between">
-    <div className="mb-3 form-check">
-      <input type="checkbox" className="form-check-input" id="Terms_and_Privacy_Check" />
-      <label className="form-check-label" htmlFor="Terms_and_Privacy_Check">Procedendo con l'ordine, accetti la Privacy Policy e i Termini e Condizioni</label>
-    </div>
-    <button type="submit" className="btn btn-dark">Conferma e Procedi con l'ordine</button>
-  </div> */}
+
 }
