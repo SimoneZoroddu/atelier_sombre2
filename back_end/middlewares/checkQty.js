64aad3b979bd3b9437a571d5f6e6cadff87e2006
angeloren92 <angeloren92@gmail.com>
2026-05-12 15:06:37 +0200

    add check quantity

--- back_end/middlewares/checkQty.js
+++ back_end/middlewares/checkQty.js
@@ -2,7 +2,23 @@ const connection = require("../data/data");
 
 const checkQty = (req, res, next) => {
 
+    const { items } = req.body;
+    const missingItems = [];
+    console.log(items);
+    items.forEach((item) => {
+        const { id, quantity } = item;
+        const query = 'SELECT * FROM '
+        const product = connection.find((product) => product.id === id);
+        if (product) {
+            if (product.quantity < quantity) {
+                missingItems.push(product.name);
+            }
+        }
+    });
+    if (missingItems.length > 0) {
+        return res.status(400).json({ error: `Missing items: ${missingItems.join(", ")}` });
+    }
     next();
 };
 
-module.exports = checkQty;
+    module.exports = checkQty;
