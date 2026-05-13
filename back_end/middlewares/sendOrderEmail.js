f23014453255ffacdb822ebf2e07a255832edd59
angeloren92 <angeloren92@gmail.com>
2026-05-13 09:52:19 +0200

    fix email

--- back_end/middlewares/sendOrderEmail.js
+++ back_end/middlewares/sendOrderEmail.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const transporter = require("../data/mailer");
-const sendOrderEmail = (req, res) => {
-    
+
+const sendOrderEmail = async (req, res) => {
+
   const { email } = req.body.order;
   try {
-    const info = transporter.sendMail({
+    const infoCustomer = await transporter.sendMail({
       from: '"Atelier Sombre" <orders@atelier-sombre.com>',
       to: email,
       subject: "Grazie per l'ordine!",
@@ -13,18 +14,8 @@ const sendOrderEmail = (req, res) => {
 <body style="margin:0; padding:0; background-color:#f5f4f0;">
 GRAZIE DELL'ORDINE
 </body>
-`
-    });
-
-    res.json({ success: true });
-
-  } catch (error) {
-    console.error("Errore invio email:", error);
-    res.status(500).json({ error: "Errore invio email ordine cliente" });
-  }
-
-  try {
-    const info = transporter.sendMail({
+`});
+    const infoSeller = await transporter.sendMail({
       from: email,
       to: '"Atelier Sombre" <orders@atelier-sombre.com>',
       subject: `Abbiamo Ricevuto un nuovo ordine da ${req.body.order.firstname} ${req.body.order.lastname}`,
@@ -41,7 +32,7 @@ ${req.body}
 
   } catch (error) {
     console.error("Errore invio email:", error);
-    res.status(500).json({ error: "Errore invio inserimento ordine" });
+    res.status(500).json({ error: "Errore invio email" });
   }
 };
 
