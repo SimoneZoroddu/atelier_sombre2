a64c8e51de25fa16f8acfbaa6e735cceb582dd1a
SimoneZoroddu <lenviur@gmail.com>
2026-05-13 14:46:08 +0200

    refactor: clean up payment method section in AppOrderCheckout component

--- front_end/src/pages/AppOrderCheckout.jsx
+++ front_end/src/pages/AppOrderCheckout.jsx
@@ -182,42 +182,41 @@ export default function AppOrderCheckout() {
         </form>
 
         {/* Payment placeholder */}
+        {/* <div className="m-3">
+        <h3>Seleziona il metodo di pagamento</h3>
         <div className="m-3">
-          <h3>Seleziona il metodo di pagamento</h3>
-          <div className="m-3">
-            {/* credit card option */}
-            <div className="form-check">
-              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1" />
-              <label className="form-check-label" htmlFor="radioDefault1">
-                <i className="bi bi-credit-card-fill"></i> Carta di credito
-              </label>
-            </div>
-            {/* apple pay option */}
-            <div className="form-check">
-              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
-              <label className="form-check-label" htmlFor="radioDefault2">
-                <i className="bi bi-apple"></i> Apple Pay
-              </label>
-            </div>
-            {/* google pay option */}
-            <div className="form-check">
-              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
-              <label className="form-check-label" htmlFor="radioDefault2">
-                <i className="payment-icon g-pay-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
-                </svg></i> Google Pay
-              </label>
-            </div>
-            {/* paypal option */}
-            <div className="form-check">
-              <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
-              <label className="form-check-label" htmlFor="radioDefault2">
-                <i className="bi bi-paypal"></i> PayPal
-              </label>
-            </div>
+      
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1" />
+            <label className="form-check-label" htmlFor="radioDefault1">
+              <i className="bi bi-credit-card-fill"></i> Carta di credito
+            </label>
+          </div>
+      
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
+            <label className="form-check-label" htmlFor="radioDefault2">
+              <i className="bi bi-apple"></i> Apple Pay
+            </label>
+          </div>
+       <div className="form-check">
+            <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
+            <label className="form-check-label" htmlFor="radioDefault2">
+              <i className="payment-icon g-pay-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
+              </svg></i> Google Pay
+            </label>
+          </div>
+      <div className="form-check">
+            <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" />
+            <label className="form-check-label" htmlFor="radioDefault2">
+              <i className="bi bi-paypal"></i> PayPal
+            </label>
           </div>
         </div>
-
+      </div> 
+      */}
+      
       </div>
     </>
   )
